5594b63babedabd52ed14447c6cd5fa7f25a297a
CarlosUribe225 <carurbe225@gmail.com>
2023-11-28 11:01:28 +0100

    added videos in VideoView

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -1,7 +1,72 @@
 <script setup lang="ts">
 </script>
+
+
 <template>
+    <h2>Videos</h2>
+    <section class="Container__Grid__Section__Videos">
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/-sKmTIix5qU?si=4dxFZdXNfcvLNtXN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Whole Loota Love (Knebworth 1979)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Ly6ZhQVnVow?si=zED0hpJkxyeeYrFQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Stairway To Heaven (Earls Court 1975)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/6tlSx0jkuLM?si=H-bLecoKLi1xaxF_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Black Dog (Madison Square Garden 1973)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/hW_WLxseq0o?si=cDRQQ7Y0IOCroFq2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Kashmir (Knebworth 1979)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2X8fDaP7ylM?si=bytuxPfkTjL4wPci" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Page & Plant: Ramble On (Birmingham 1995)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/E-Xna3ny6KY?si=a-DBvMzrH42ZRNRy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Good Times Bad Times (London 2007)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/RlNhD0oS5pk?si=hbGl-jwmKZWw8d_j" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Immigrant Song (Sydney 1972)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/emzih-d4Cs8?si=P71RbzROYdM-_jua" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Page & Plant: When the Levee Breaks(Milwaukee, 1995)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/YkMF0tHyEC0?si=6a_UCGJo8hDetiiW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Rock and Roll (Landover 1977)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/KwG9iRFmY1I?si=8UNx39uuZKzhf1Ua" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Misty Mountain Hop (Madison Square Garden 1973)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/TvG4VGnjpsg?si=1unb6UieBk3G10zf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>Over the Hills and Far Away (London 1975)</h3>
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/6XIQR4p30Wo?si=wuzVIazF4l4Z_h2i" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <h3>The Ocean (Madison Square Garden 1973)</h3>
+        </div>
+
+    </section>
 </template>
+
+
 <style lang="scss">
 @import "../assets/styles.scss";
+
+.Container__Grid__Section__Videos {
+    margin-left: 1rem;
+    width: 100%;
+    height: auto;
+    gap: 1rem;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(45%, 1fr));
+}
 </style>
