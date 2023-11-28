3de674cd16c52330f6eb70d05a2f0a302a483ae6
CarlosUribe225 <carurbe225@gmail.com>
2023-11-28 04:40:58 +0100

    finished styles in ShopView

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <main>
+    <article>
         <section class="Title__And__Cart_Img">
             <h2>Shop</h2>
             <div class="Cart__Img">
@@ -11,45 +11,83 @@
             </div>
         </section>
 
-        <section>
-            <div>
+        <section class="Grid__Container__Shop">
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop1.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
-            <div>
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop2.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
-            <div>
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop3.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
-            <div>
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop4.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
-            <div>
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop5.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
-            <div>
+                <div class="Container__Card__Add__To__Cart">
+                    <img src="../assets/img_shop6.png" alt="">
+                    <p>Led Zeppelin | Mothership</p>
+                    <div class="Container__Price__And__Button">
+                        <h3>$22.75</h3>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
 
-            </div>
             
         </section>
 
-    </main>
+    </article>
 </template>
 
 <style lang="scss">
 @import "../assets/styles.scss";
 
-main {
-    background: #1E1E1E;
+article {
+    background: #2C2C2C;
+    min-height: auto;
 
     .Title__And__Cart_Img {
         padding: 0.1rem;
         display: flex;
-        justify-content: space-between;
+        justify-content:space-between;
         align-items: center;
+        margin-bottom: 1rem;
 
         h2 {
-            font-size: 3rem;
+            font-size: 2rem;
             color: white;
             margin: 0 0.5rem;
         }
@@ -68,6 +106,54 @@ main {
                 width: 60px;
                 height: 60px;
                 margin: 0 0.5rem;
+            }
+        }
+    }
+
+    .Grid__Container__Shop {
+        width: 100%;
+        height: auto;
+        gap: 1rem;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+        
+
+        .Container__Card__Add__To__Cart {
+            width: 100%;
+            margin-bottom: 1.5rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+
+            p {
+                color: white;
+                font-size: 1rem;
+                padding: 0.5rem;
+                
+            }
+        
+
+            .Container__Price__And__Button {
+                display: flex;
+                justify-content:space-between;
+                align-items: center;
+                width:60%;
+                background-color:#2C2C2C;
+                
+
+                h3 {
+                    font-size: 1.5rem;
+                    color: white;
+                }
+
+                button {
+                    background-color: #2C2C2B;
+                    color: white;
+                    width: auto;
+                    height: 1.5rem;
+                    padding: 5px;
+                }
             }
         }
     }
